52c6ec42d636937c9f88445ffd55dfea6589e6ed
izmi.komar <komar.izmi@gmail.com>
2024-08-19 23:32:27 +0700

    chore: Update dependencies and configuration files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import compression from '@fastify/compress'
+import {
+	FastifyAdapter,
+	NestFastifyApplication,
+} from '@nestjs/platform-fastify'
+import { env } from 'process'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(),
+	)
+	app.enableCors()
+	app.register(compression, { encodings: ['gzip', 'deflate'] })
+	app.useGlobalPipes(new ValidationPipe())
+	await app.listen(env.PORT)
 }
 bootstrap()
