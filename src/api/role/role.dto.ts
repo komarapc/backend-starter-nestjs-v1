281d37b88cdad19537e594d0549f95b64d4521c2
izmi.komar <komar.izmi@gmail.com>
2024-08-21 00:02:45 +0700

    [chore][update] remove unused imports

--- src/api/role/role.dto.ts
+++ src/api/role/role.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, ApiQuery } from '@nestjs/swagger'
+import { ApiProperty } from '@nestjs/swagger'
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator'
 import { Type } from 'class-transformer'
 
