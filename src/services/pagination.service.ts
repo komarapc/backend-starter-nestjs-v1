49df7be81f7b8184e23f2c4bd9d0afa143821892
izmi.komar <komar.izmi@gmail.com>
2024-08-21 00:01:27 +0700

    [feat] add function to get meta pagination

--- src/services/pagination.service.ts
+++ src/services/pagination.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common'
 
+type MetaPage = {
+	page: number
+	limit: number
+	total_data: number
+}
+
 @Injectable()
 export class PaginationService {
 	public getPagination(
@@ -7,6 +13,16 @@ export class PaginationService {
 		limit: number,
 	): { skip: number; take: number } {
 		const skip = (page - 1) * limit
-		return { skip, take: limit }
+		return { skip, take: Number(limit) }
+	}
+
+	public getMetaPage(props: MetaPage) {
+		const { page, limit, total_data } = props
+		return {
+			total_data,
+			total_page: Math.ceil(total_data / limit),
+			current_page: page,
+			limit,
+		}
 	}
 }
