e8c32220e2e10e12ebf07f17ee0835a48f999e99
izmi.komar <komar.izmi@gmail.com>
2024-08-22 05:19:57 +0700

    feat(auth): add authentication module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config'
 import { UserModule } from './api/user/user.module'
 import { RoleModule } from './api/role/role.module'
 import { HasRoleModule } from './api/has-role/has-role.module'
+import { AuthModule } from './api/auth/auth.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), UserModule, RoleModule, HasRoleModule],
+	imports: [ConfigModule.forRoot(), UserModule, RoleModule, HasRoleModule, AuthModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
