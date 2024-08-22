48ae6e697814e71fca6cce9c5367cdfe04fc6ef8
izmi.komar <komar.izmi@gmail.com>
2024-08-23 03:42:02 +0700

    remove migration

--- prisma/migrations/20240822180858_modify_columns_in_table_auth_logs/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `location` on the `auth_logs` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "auth_logs" DROP COLUMN "location";
